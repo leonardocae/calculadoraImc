42f2b5479151ddf4ce0c8bb7c157ac9971592045
leonardocae <leonardocae17@gmail.com>
2021-08-27 15:16:41 -0300

    fix Resultado estão de acordo com a tabela - Calculo IMC modificado de metros para centímetro - add Placeholder nos inputs

--- JS/script.js
+++ JS/script.js
@@ -44,7 +44,7 @@ function getResultadoImc (imc) {
 }
 
 function getImc (peso, altura) {
-    const imc = peso / altura ** 2;
+    const imc = peso / (altura / 100) ** 2;
     return imc.toFixed(2);
 
 }
